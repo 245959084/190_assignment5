cc1c392642908277f95733f3e6ddcf4f75a4f07b
JingJun Ni <jin010@ucsd.edu>
2026-05-06 00:39:57 +0000

    fix error

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 def main():
     print("Hello from 190-assignment5!")
-import os
-
-x = 5
-try:
-    print(1/0)
-except:
-    print("error")
-print("hello")
 
 if __name__ == "__main__":
     main()
